624af625ef6e33e1bd74a8659f8859dfb03e44a1
Jin Choi <jinchoi721@gmail.com>
2015-12-02 10:36:26 -0600

    first commit

---
+++ initial_setup/pre-install.sh
@@ -0,0 +1,8 @@
+apt-get update
+apt-get -y install make gcc git emacs24
+apt-get -y install python-dev libxml2-dev libxslt-dev python-pip
+apt-get install python2.7-dev python-virtualenv g++
+apt-get install default-jre
+easy_install -U setuptools
+pip install screed
+pip install khmer
