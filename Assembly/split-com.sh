fc7a439ec6b8fa28bc691929d2e44105ad6e4e2e
Jin Choi <jinchoi721@gmail.com>
2015-12-03 13:56:21 -0600

    update

---
+++ Assembly/split-com.sh
@@ -0,0 +1,1 @@
+for file in *.fastq;do echo "paste - - - - < $file | tee >(awk 'BEGIN{FS=\"\t\";OFS=\"\n\"}{if (match(\$1,\" 1:N\"))print \$1,\$2,\$3,\$4}' > $file._R1.fastq) | awk 'BEGIN{FS=\"\t\"; OFS=\"\n\"}{if (match(\$1,\" 2:N\"))print \$1,\$2,\$3,\$4}' > $file._R2.fastq";done > split.sh
