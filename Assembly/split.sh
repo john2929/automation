04474dc020403a51d6d4f4f991e7f6691eeb8e10
Jin Choi <jinchoi721@gmail.com>
2016-11-03 15:20:35 -0500

    update

--- Assembly/split.sh
+++ Assembly/split.sh
@@ -1,5 +1,5 @@
 gunzip *.fastq.gz
 for file in *.fastq;do echo "paste - - - - < $file | tee >(awk 'BEGIN{FS=\"\t\";OFS=\"\n\"}{if (match(\$1,\" 1:N\"))print \$1,\$2,\$3,\$4}' > $file._R1.fastq) | awk 'BEGIN{FS=\"\t\"; OFS=\"\n\"}{if (match(\$1,\" 2:N\"))print \$1,\$2,\$3,\$4}' > $file._R2.fastq";done > split-command.sh
-cat split-command.sh | prallel
+cat split-command.sh | parallel
 gzip *_R1.fastq
 gzip *_R2.fastq
